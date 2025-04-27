1042af677b46e16cfe8288ac5c5a2b438c3a5c87
adhfac <mdhafaadjiesaputra_2327250081@mhs.mdp.ac.id>
2025-04-27 14:31:39 +0700

    Update profile_screen.dart

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -12,9 +12,26 @@ class ProfileScreen extends StatelessWidget {
           IconButton(
               onPressed: () {
                 Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const SettingScreen()));
+                  context,
+                  PageRouteBuilder(
+                    pageBuilder: (context, animation, secondaryAnimation) =>
+                        const SettingScreen(),
+                    transitionsBuilder:
+                        (context, animation, secondaryAnimation, child) {
+                      // Menambahkan efek slide dari kanan ke kiri
+                      const begin = Offset(1.0, 0.0); // mulai dari kanan
+                      const end = Offset.zero; // berakhir di posisi normal
+                      var tween = Tween(begin: begin, end: end).chain(
+                          CurveTween(
+                              curve: Curves
+                                  .easeInOut));
+                      var offsetAnimation = animation.drive(tween);
+
+                      return SlideTransition(
+                          position: offsetAnimation, child: child);
+                    },
+                  ),
+                );
               },
               icon: const Icon(Icons.settings_outlined))
         ],
